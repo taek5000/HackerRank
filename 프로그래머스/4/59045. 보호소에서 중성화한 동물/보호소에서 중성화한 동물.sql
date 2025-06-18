b498c554c26e455e8af8a4e24d082551e5e9d7e5
taek5000 <132982650+taek5000@users.noreply.github.com>
2025-06-18 22:45:47 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -0,0 +1,11 @@
+-- 코드를 입력하세요
+SELECT A.ANIMAL_ID
+     , A.ANIMAL_TYPE
+     , A.NAME
+FROM ANIMAL_INS A
+INNER JOIN ANIMAL_OUTS B
+            ON A.ANIMAL_ID = B.ANIMAL_ID
+WHERE 1 = 1
+    AND A.SEX_UPON_INTAKE LIKE '%Intact%'
+    AND B.SEX_UPON_OUTCOME IN ('Spayed Female', 'Neutered Male')
+ORDER BY A.ANIMAL_ID
